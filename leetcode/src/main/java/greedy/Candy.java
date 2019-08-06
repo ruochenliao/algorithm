415971873f1638b78aa912a0e818677f36c91f80
john <lrc272317227@gmail.com>
2019-08-06 23:42:14 +0800

    复习

--- leetcode/src/main/java/greedy/Candy.java
+++ leetcode/src/main/java/greedy/Candy.java
@@ -4,16 +4,14 @@ public class Candy {
     public int candy(int[] ratings) {
         if (ratings.length == 0) return 0;
         int ret = 1;
-        int up = 0, down = 0, peak = 0;
+        int up = 0, down = 0;
         int candyGet = 1;
         for (int i = 1; i < ratings.length; i++) {
             if (ratings[i - 1] < ratings[i]) {
-                peak = ++up;
                 down = 0;
                 candyGet = 1 + up;
                 ret += 1 + up;
             } else if (ratings[i - 1] == ratings[i])  {
-                peak = up = down = 0;
                 ret += 1;
                 candyGet = 1;
             } else {
--- leetcode/src/main/java/greedy/QueueReconstructByHeight.java
+++ leetcode/src/main/java/greedy/QueueReconstructByHeight.java
@@ -5,11 +5,27 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * 思路: 如果 k 值越低的人越应该站到前面, 但是如果后面的人比前面的人高, 后面的 k 值可以比前面的低
  *
- * 如果能把人按高度从高往低排序, 并且高度相同的按 k 值从低往高排序,
- * 那么遍历这个排好序的数组, h 越高且k 值越低的, 越先被遍历到;
- * 如果把 k 值相同的 h 低的数组插入到 h 高的数组前, 那么 k 值相同的就会从低往高排序
+ * Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k),
+ * where h is the height of the person and k is the number of people in front of this person who have a height greater
+ * than or equal to h. Write an algorithm to reconstruct the queue.
+ *
+ * Note:
+ * The number of people is less than 1,100.
+ *
+ * Input:
+ * [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+ *
+ * Output:
+ * [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+ *
+ * 思路: 同样高的情况下, k 值越小越应该在前面;
+ *      如果矮子插入队列中, 对高个不会有影响;
+ *
+ * 贪心策略: 先插入高的, 如果高度相同,则先插入 k 值交小的;
+ *
+ *
+ *
  */
 public class QueueReconstructByHeight {
     public int[][] reconstructQueue(int[][] people) {
