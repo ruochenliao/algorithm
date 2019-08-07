4dc30785486fb3db9d6797aa93f401c22a09511f
john <lrc272317227@gmail.com>
2019-08-07 09:20:12 +0800

    gas station

--- leetcode/src/main/java/greedy/GasStation.java
+++ leetcode/src/main/java/greedy/GasStation.java
@@ -51,22 +51,17 @@ package greedy;
  */
 public class GasStation {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int tank = 0;
-        for(int i = 0; i < gas.length;i++){
-            tank += gas[i] - cost[i];
-        }
-        if(tank < 0){
-            return -1;
-        }
-        tank = 0;
         int start = 0;
+        int tank = 0;
+        int total = 0;
         for(int i = 0; i < gas.length; i++){
             tank += gas[i] - cost[i];
+            total += gas[i] - cost[i];
             if(tank < 0){
-                tank = 0;
                 start = i+1;
+                tank = 0;
             }
         }
-        return start;
+        return total >= 0?start:-1;
     }
 }
