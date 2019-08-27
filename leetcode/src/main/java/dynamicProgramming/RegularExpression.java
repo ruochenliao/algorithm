bed13b099f3cec449501db938a1287f383e6ba04
john <lrc272317227@gmail.com>
2019-08-27 09:39:35 +0800

    recursion

--- leetcode/src/main/java/dynamicProgramming/RegularExpression.java
+++ leetcode/src/main/java/dynamicProgramming/RegularExpression.java
@@ -2,50 +2,50 @@ package dynamicProgramming;
 
 /**
  * 10. Regular Expression Matching
- Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
-
- '.' Matches any single character.
- '*' Matches zero or more of the preceding element.
- The matching should cover the entire input string (not partial).
-
- Note:
-
- s could be empty and contains only lowercase letters a-z.
- p could be empty and contains only lowercase letters a-z, and characters like . or *.
- Example 1:
-
- Input:
- s = "aa"
- p = "a"
- Output: false
- Explanation: "a" does not match the entire string "aa".
- Example 2:
-
- Input:
- s = "aa"
- p = "a*"
- Output: true
- Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
- Example 3:
-
- Input:
- s = "ab"
- p = ".*"
- Output: true
- Explanation: ".*" means "zero or more (*) of any character (.)".
- Example 4:
-
- Input:
- s = "aab"
- p = "c*a*b"
- Output: true
- Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore, it matches "aab".
- Example 5:
-
- Input:
- s = "mississippi"
- p = "mis*is*p*."
- Output: false
+ * Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+ * <p>
+ * '.' Matches any single character.
+ * '*' Matches zero or more of the preceding element.
+ * The matching should cover the entire input string (not partial).
+ * <p>
+ * Note:
+ * <p>
+ * s could be empty and contains only lowercase letters a-z.
+ * p could be empty and contains only lowercase letters a-z, and characters like . or *.
+ * Example 1:
+ * <p>
+ * Input:
+ * s = "aa"
+ * p = "a"
+ * Output: false
+ * Explanation: "a" does not match the entire string "aa".
+ * Example 2:
+ * <p>
+ * Input:
+ * s = "aa"
+ * p = "a*"
+ * Output: true
+ * Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+ * Example 3:
+ * <p>
+ * Input:
+ * s = "ab"
+ * p = ".*"
+ * Output: true
+ * Explanation: ".*" means "zero or more (*) of any character (.)".
+ * Example 4:
+ * <p>
+ * Input:
+ * s = "aab"
+ * p = "c*a*b"
+ * Output: true
+ * Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore, it matches "aab".
+ * Example 5:
+ * <p>
+ * Input:
+ * s = "mississippi"
+ * p = "mis*is*p*."
+ * Output: false
  */
 public class RegularExpression {
     //recursion method
@@ -53,11 +53,10 @@ public class RegularExpression {
         if (pattern.isEmpty()) return text.isEmpty();
         boolean firstMatch = (!text.isEmpty() &&
                 (pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.'));
-        if(pattern.length() >= 2 && pattern.charAt(1) == '*'){
+        if (pattern.length() >= 2 && pattern.charAt(1) == '*') {
             return (firstMatch && isMatchRecursion(text.substring(1), pattern))
                     || isMatchRecursion(text, pattern.substring(2));
-        }
-        else{
+        } else {
             return firstMatch && isMatchRecursion(text.substring(1), pattern.substring(1));
         }
     }
@@ -78,18 +77,18 @@ public class RegularExpression {
             return memo[i][j] == Result.TRUE;
         }
         boolean ans;
-        if (j == pattern.length()){
+        if (j == pattern.length()) {
             ans = i == text.length();
-        } else{
+        } else {
             boolean first_match = (i < text.length() &&
                     (pattern.charAt(j) == text.charAt(i) ||
                             pattern.charAt(j) == '.'));
 
-            if (j + 1 < pattern.length() && pattern.charAt(j+1) == '*'){
-                ans = (dp(i, j+2, text, pattern) ||
-                        first_match && dp(i+1, j, text, pattern));
+            if (j + 1 < pattern.length() && pattern.charAt(j + 1) == '*') {
+                ans = (dp(i, j + 2, text, pattern) ||
+                        first_match && dp(i + 1, j, text, pattern));
             } else {
-                ans = first_match && dp(i+1, j+1, text, pattern);
+                ans = first_match && dp(i + 1, j + 1, text, pattern);
             }
         }
         memo[i][j] = ans ? Result.TRUE : Result.FALSE;
@@ -101,18 +100,59 @@ public class RegularExpression {
         boolean[][] dp = new boolean[text.length() + 1][pattern.length() + 1];
         dp[text.length()][pattern.length()] = true;
 
-        for (int i = text.length(); i >= 0; i--){
-            for (int j = pattern.length() - 1; j >= 0; j--){
+        for (int i = text.length(); i >= 0; i--) {
+            for (int j = pattern.length() - 1; j >= 0; j--) {
                 boolean first_match = (i < text.length() &&
                         (pattern.charAt(j) == text.charAt(i) ||
                                 pattern.charAt(j) == '.'));
-                if (j + 1 < pattern.length() && pattern.charAt(j+1) == '*'){
-                    dp[i][j] = dp[i][j+2] || first_match && dp[i+1][j];
+                if (j + 1 < pattern.length() && pattern.charAt(j + 1) == '*') {
+                    dp[i][j] = dp[i][j + 2] || first_match && dp[i + 1][j];
                 } else {
-                    dp[i][j] = first_match && dp[i+1][j+1];
+                    dp[i][j] = first_match && dp[i + 1][j + 1];
                 }
             }
         }
         return dp[0][0];
+    }
+
+    enum BinaryResult {
+        TRUE,
+        FALSE;
+    }
+
+    /**
+     * 递归算法
+     *
+     * @param text
+     * @param pattern
+     * @return
+     */
+    public boolean isMatchRecursive(String text, String pattern) {
+        BinaryResult[][] dp = new BinaryResult[text.length() + 1][pattern.length() + 1];
+        return isMatchRecursive(0, 0, text, pattern, dp);
+    }
+
+    public boolean isMatchRecursive(int i, int j, String text, String pattern, BinaryResult[][] dp) {
+        //是否已经计算过了
+        if (dp[i][j] != null) {
+            return dp[i][j].equals(BinaryResult.TRUE);
+        }
+        boolean ans;
+        //结果兜底
+        if (j == pattern.length()) {
+            ans = i == text.length();
+        }
+        else {
+            //递归公式
+            boolean firstMatch = i < text.length() && (text.charAt(i) == pattern.charAt(j) || pattern.charAt(j) == '.');
+            if (pattern.length() > j + 1 && pattern.charAt(j + 1) == '*') {
+                ans = firstMatch && isMatchRecursive(i + 1, j, text, pattern, dp) || isMatchRecursive(i, j + 2, text, pattern, dp);
+            } else {
+                ans = firstMatch && isMatchRecursive(i + 1, j + 1, text, pattern, dp);
+            }
+        }
+        //record
+        dp[i][j] = ans ? BinaryResult.TRUE:BinaryResult.FALSE;
+        return ans;
     }
 }
--- leetcode/src/test/java/dynamicProgramming/TestRegularExpressionMatching.java
+++ leetcode/src/test/java/dynamicProgramming/TestRegularExpressionMatching.java
@@ -3,7 +3,10 @@ package dynamicProgramming;
 public class TestRegularExpressionMatching {
     public static void main(String[] args){
         RegularExpression expression = new RegularExpression();
-        boolean result = expression.isMatchRecursion("aaa", "a*a");
+        boolean result = expression.isMatch("aa", ".*");
         System.out.println(result);
+
+        boolean recursiveResult = expression.isMatchRecursive("aa", ".*");
+        System.out.println(recursiveResult);
     }
 }
