bd71cf31fd717dec602088c01a5dd7c8696954d7
john <lrc272317227@gmail.com>
2019-09-05 09:30:57 +0800

    dp 写法

--- leetcode/src/main/java/dynamicProgramming/LongestParenthese.java
+++ leetcode/src/main/java/dynamicProgramming/LongestParenthese.java
@@ -8,6 +8,18 @@ public class LongestParenthese {
      * @param s
      * @return
      */
+    public int longestValidParenthesesBruteForce(String s) {
+        int maxlen = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j += 1) {
+                if (isValid(s.substring(i, j))) {
+                    maxlen = Math.max(maxlen, j - i);
+                }
+            }
+        }
+        return maxlen;
+    }
+
     private boolean isValid(String s) {
         Stack<Character> stack = new Stack<Character>();
         for (int i = 0; i < s.length(); i++) {
@@ -22,108 +34,38 @@ public class LongestParenthese {
         return stack.empty();
     }
 
-    public int longestValidParentheses(String s) {
-        int maxlen = 0;
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i + 1; j <= s.length(); j += 1) {
-                if (isValid(s.substring(i, j))) {
-                    maxlen = Math.max(maxlen, j - i);
-                }
-            }
-        }
-        return maxlen;
-    }
+
 
     /**
      * dp
+     * dp[i] 代表 [ i - j, i] 满足 最长括号的长度
+     * 如果遇到 ")" 则认为是结尾
+     * if(s.charAt(i-1) == '('){
+     *     dp[i] = 2 + dp[i-2];
+     * }
+     * if(s.charAt(i-dp[i-1]-1) == '('){
+     *     dp[i] = 2 + dp[i-1] + dp[i-d[i-1]-2];
+     * }
+     * 最后比较出最大值
+     * maxLength = Math.max(maxLength, dp[i]);
      * @param s
      * @return
      */
-    public int longestValidParentheses(String s) {
-        int maxans = 0;
-        int dp[] = new int[s.length()];
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) == ')') {
-                if (s.charAt(i - 1) == '(') {
-                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
-                } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
-                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+    public int longestValidParenthesesDp(String s) {
+        int[] dp = new int[s.length()];
+        int maxLength = 0;
+        for(int i = 0; i < s.length();  i++){
+            if(s.charAt(i) == ')'){
+                if(i - 1 >= 0 && s.charAt(i-1) == '('){
+                    dp[i] = (i - 2 >= 0? dp[i-2] : 0) + 2;
                 }
-                maxans = Math.max(maxans, dp[i]);
-            }
-        }
-        return maxans;
-    }
-
-    /**
-     * stack
-     */
-    public int longestValidParentheses(String s) {
-        int maxans = 0;
-        Stack<Integer> stack = new Stack<>();
-        stack.push(-1);
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                stack.push(i);
-            } else {
-                stack.pop();
-                if (stack.empty()) {
-                    stack.push(i);
-                } else {
-                    maxans = Math.max(maxans, i - stack.peek());
+                else if(i - 1 > 0 && i - dp[i-1] - 1 >= 0 && s.charAt(i - dp[i-1] - 1) == '('){
+                    dp[i] = 2+ dp[i-1] + ((i - dp[i-1] - 2) >= 0? dp[i - dp[i-1] - 2]:0);
                 }
             }
+            maxLength = Math.max(maxLength, dp[i]);
         }
-        return maxans;
+        return maxLength;
     }
-    /**
-     *Approach 4: Without extra space
-     * Algorithm
-     *
-     * In this approach, we make use of two counters leftleft and rightright. First, we start traversing the string from the left towards the right and for every \text{‘(’}‘(’ encountered, we increment the leftleft counter and for every \text{‘)’}‘)’ encountered, we increment the rightright counter. Whenever leftleft becomes equal to rightright, we calculate the length of the current valid string and keep track of maximum length substring found so far. If rightright becomes greater than leftleft we reset leftleft and rightright to 00.
-     *
-     * Next, we start traversing the string from right to left and similar procedure is applied.
-     */
-    public int longestValidParentheses(String s) {
-        int left = 0, right = 0, maxlength = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                left++;
-            } else {
-                right++;
-            }
-            if (left == right) {
-                maxlength = Math.max(maxlength, 2 * right);
-            } else if (right >= left) {
-                left = right = 0;
-            }
-        }
-        left = right = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) == '(') {
-                left++;
-            } else {
-                right++;
-            }
-            if (left == right) {
-                maxlength = Math.max(maxlength, 2 * left);
-            } else if (left >= right) {
-                left = right = 0;
-            }
-        }
-        return maxlength;
-    }
-    /**
-     * dp
-     * dp[i] 表示 0 到 i 最长的 parenthese
-     */
-    public int longestValidParenthesesDp(String s){
-        Integer[] dp = new Integer[s.length() + 1];
-        for(int i = 0; i < s.length()+1;  i++){
-             = s.charAt(i);
-            if(){
 
-            }
-        }
-    }
 }
--- leetcode/src/test/java/dynamicProgramming/TestLongestParanthese.java
+++ leetcode/src/test/java/dynamicProgramming/TestLongestParanthese.java
@@ -4,7 +4,9 @@ public class TestLongestParanthese {
     public static void main(String[] args){
         LongestParenthese obj = new LongestParenthese();
         String str = "()()";
-        String result1 = obj.longestValidParentheses(str):
+        int result1 = obj.longestValidParenthesesBruteForce(str);
         System.out.println(result1);
+        int result2 = obj.longestValidParenthesesDp(str);
+        System.out.println(result2);
     }
 }
