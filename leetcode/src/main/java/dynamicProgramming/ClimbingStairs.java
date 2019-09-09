8534490aa2bbb2a97fd419d16444f0e2525f739c
john <lrc272317227@gmail.com>
2019-09-09 21:51:55 +0800

    climbStairs

--- leetcode/src/main/java/dynamicProgramming/ClimbingStairs.java
+++ leetcode/src/main/java/dynamicProgramming/ClimbingStairs.java
@@ -2,15 +2,27 @@ package dynamicProgramming;
 
 public class ClimbingStairs {
     /**
-     * recursive dp
-     * top to bottom
+     * recursive
      * @param n
      * @return
      */
-    public int climbStairs(int n) {
-        return climbStairsRecursive(int remain, new Integer[n])
+    public int climbStairsRecursive(int n) {
+        if(n == 0){
+            return 1;
+        }
+        if(n < 0){
+            return 0;
+        }
+        return climbStairsRecursive(n-1) + climbStairsRecursive( n - 2);
     }
-    public int climbStairsRecursive(int remain, Integer[] dp){
+    /**
+     * recursive dp
+     * top to bottom
+     * @param remain
+     * @return
+     */
+
+    public int climbStairsRecursiveDp(int remain, Integer[] dp){
         if(remain == 0){
             return 1;
         }
@@ -20,10 +32,24 @@ public class ClimbingStairs {
         if(dp[remain] != null){
             return dp[remain];
         }
-        int steps = climbStairsRecursive(remain - 1, dp) + climbStairsRecursive(remain - 2, dp);
+        int steps = climbStairsRecursiveDp(remain - 1, dp) + climbStairsRecursiveDp(remain - 2, dp);
         dp[remain] = steps;
         return steps;
     }
 
+    /**
+     * iterative
+     * @param n
+     * @return
+     */
+    public int climbStairsIterativeDp(int n) {
+        int[] steps = new int[n+1];
+        steps[0] = 1;
+        steps[1] = 1;
+        for(int i = 2; i < n+1; i++){
+            steps[i] = steps[i-1] + steps[i-2];
+        }
+        return steps[n];
+    }
 }
 
