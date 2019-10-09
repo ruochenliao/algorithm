524aaec87236dad1a40d91cda914456685a8bf8b
john <lrc272317227@gmail.com>
2019-10-09 09:02:58 +0800

    in order traverse

--- leetcode/src/main/java/dfs/ValidateBinarySearchTree.java
+++ leetcode/src/main/java/dfs/ValidateBinarySearchTree.java
@@ -2,6 +2,9 @@ package dfs;
 
 import Utils.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Stack;
+
 /**
  * 98. Validate Binary Search Tree
  * Medium
@@ -43,29 +46,126 @@ import Utils.TreeNode;
  * Explanation: The root node's value is 5 but its right child's value is 4.
  */
 public class ValidateBinarySearchTree {
-    public boolean isValidBST(TreeNode root) {
+    /**
+     * recursive
+     * @param root
+     * @return
+     */
+    public boolean isValidBST(TreeNode root){
+        return isValidBST(root, null, null);
+    }
+    public boolean isValidBST(TreeNode root, Integer lower, Integer upper) {
         if(root == null){
             return true;
         }
-        if(root.left != null){
-            boolean isLeftMatch = isValidBST(root.left);
-            if(!isLeftMatch){
-                return false;
-            }
-            if(root.left.val >= root.val){
-                return false;
-            }
+        if(lower != null && root.val <= lower){
+            return false;
         }
-        if(root.right != null){
-            boolean isRightMatch = isValidBST(root.right);
-            if(!isRightMatch){
-                return false;
-            }
-            if(root.val >= root.right.val){
-                return false;
-            }
+        if(upper != null && root.val >= upper){
+            return false;
+        }
+        if(!isValidBST(root.left, lower, root.val)){
+            return false;
+        }
+        if(!isValidBST(root.right, root.val, upper)){
+            return false;
         }
         return true;
     }
 
+    /**
+     * iterative bfs
+     * @param treeNode
+     * @return
+     */
+    private static LinkedList<TreeNode> curList = new LinkedList<>();
+    private static LinkedList<Integer> lowerList = new LinkedList<>();
+    private static LinkedList<Integer> upperList = new LinkedList<>();
+    public boolean isValidBSTIterative(TreeNode treeNode){
+        insertNode(treeNode, null, null);
+        while(!curList.isEmpty()){
+            TreeNode cur = curList.remove();
+            Integer lowerVal = lowerList.remove();
+            Integer upperVal = upperList.remove();
+            if(cur == null){
+                continue;
+            }
+            if(lowerVal != null && cur.val <= lowerVal){
+                return false;
+            }
+            if(upperVal != null && cur.val >= upperVal){
+                return false;
+            }
+            insertNode(cur.left, lowerVal, cur.val);
+            insertNode(cur.right, cur.val, upperVal);
+        }
+        return true;
+    }
+
+    private void insertNode(TreeNode treeNode, Integer lower, Integer upper) {
+        curList.add(treeNode);
+        lowerList.add(lower);
+        upperList.add(upper);
+    }
+
+    /**
+     * iterative dfs
+     * @param treeNode
+     * @return
+     */
+    LinkedList<TreeNode> curList1 = new LinkedList<>();
+    LinkedList<Integer> lowerList1 = new LinkedList<>();
+    LinkedList<Integer> upperList1 = new LinkedList<>();
+    public boolean isValidBSTIterativeBfs(TreeNode treeNode){
+        insertNode1(treeNode, null, null);
+        while(!curList1.isEmpty()){
+            TreeNode cur = curList1.poll();
+            Integer lowerVal = lowerList1.poll();
+            Integer upperVal = upperList1.poll();
+            if(cur == null){
+                continue;
+            }
+            if(lowerVal != null && cur.val <= lowerVal){
+                return false;
+            }
+            if(upperVal != null && cur.val >= upperVal){
+                return false;
+            }
+            System.out.println(cur.val);
+            insertNode1(cur.left, lowerVal, cur.val);
+            insertNode1(cur.right, cur.val, upperVal);
+        }
+        return true;
+    }
+
+    private void insertNode1(TreeNode treeNode, Integer lower, Integer upper) {
+        curList1.add(treeNode);
+        lowerList1.add(lower);
+        upperList1.add(upper);
+    }
+
+    /**
+     * inOrder traverse
+     * @param root
+     * @return
+     */
+    public boolean isValidBSTInOrder(TreeNode root) {
+        Stack<TreeNode> stack = new Stack();
+        double inorder = - Double.MAX_VALUE;
+
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            // If next element in inorder traversal
+            // is smaller than the previous one
+            // that's not BST.
+            if (root.val <= inorder) return false;
+            inorder = root.val;
+            root = root.right;
+        }
+        return true;
+    }
 }
--- leetcode/src/test/java/bfs/TestValidateBinarySearchTree.java
+++ leetcode/src/test/java/bfs/TestValidateBinarySearchTree.java
@@ -5,11 +5,17 @@ import dfs.ValidateBinarySearchTree;
 
 public class TestValidateBinarySearchTree {
     public static void main(String[] args){
-        TreeNode treeNode = new TreeNode(2);
-        treeNode.left = new TreeNode(1);
-        treeNode.right = new TreeNode(3);
+        TreeNode treeNode = new TreeNode(-30);
+        treeNode.left = new TreeNode(-31);
+        treeNode.right = new TreeNode(-22);
+        treeNode.right.left = new TreeNode(-25);
+        treeNode.right.right = new TreeNode(80);
         ValidateBinarySearchTree searchTree = new ValidateBinarySearchTree();
         boolean result = searchTree.isValidBST(treeNode);
         System.out.println(result);
+        boolean result2 = searchTree.isValidBSTIterative(treeNode);
+        System.out.println(result2);
+        boolean result3 = searchTree.isValidBSTIterativeBfs(treeNode);
+        System.out.println(result3);
     }
 }
