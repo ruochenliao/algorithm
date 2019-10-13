ec428b57b84278d889f4cc46b789f13be8e2b2f0
john <lrc272317227@gmail.com>
2019-10-13 11:02:22 +0800

    bfs easy 题

---
+++ leetcode/src/main/java/dfs/SameTree.java
@@ -0,0 +1,67 @@
+package dfs;
+
+import Utils.TreeNode;
+
+/**
+ * 100. Same Tree
+ * Easy
+ *
+ * 1344
+ *
+ * 45
+ *
+ * Favorite
+ *
+ * Share
+ * Given two binary trees, write a function to check if they are the same or not.
+ *
+ * Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+ *
+ * Example 1:
+ *
+ * Input:     1         1
+ *           / \       / \
+ *          2   3     2   3
+ *
+ *         [1,2,3],   [1,2,3]
+ *
+ * Output: true
+ * Example 2:
+ *
+ * Input:     1         1
+ *           /           \
+ *          2             2
+ *
+ *         [1,2],     [1,null,2]
+ *
+ * Output: false
+ * Example 3:
+ *
+ * Input:     1         1
+ *           / \       / \
+ *          2   1     1   2
+ *
+ *         [1,2,1],   [1,1,2]
+ *
+ * Output: false
+ */
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null){
+            return true;
+        }
+        if(p == null || q == null){
+            return false;
+        }
+        if(p.val != q.val){
+            return false;
+        }
+        if(!isSameTree(p.left, q.left)){
+            return false;
+        }
+        if(!isSameTree(p.right, q.right)){
+            return false;
+        }
+        return true;
+    }
+}
---
+++ leetcode/src/main/java/dfs/SymmetricTree.java
@@ -0,0 +1,30 @@
+package dfs;
+
+import Utils.TreeNode;
+
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+    private boolean isSymmetric(TreeNode left, TreeNode right){
+        if(left == null && right == null){
+            return true;
+        }
+        if(left == null || right == null){
+            return false;
+        }
+        if(left.val != right.val){
+            return false;
+        }
+        if(!isSymmetric(left.left, right.right)){
+            return false;
+        }
+        if(!isSymmetric(left.right, right.left)){
+            return false;
+        }
+        return true;
+    }
+}
