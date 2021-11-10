eb58dceb8d1a054feb34bf7317115027d954c4b8
john <lrc272317227@gmail.com>
2021-11-10 21:41:36 +0800

    记录在线人数

---
+++ leetcode/src/main/java/dynamicProgramming/MinusTarget.java
@@ -0,0 +1,69 @@
+package dynamicProgramming;
+
+import javafx.util.Pair;
+
+import java.util.*;
+
+public class MinusTarget {
+    public static void main(String[] args) {
+        int[] intArray = new int[]{2, 3, 5, 2, 8, 6};
+        int target = 3;
+        List<Pair<Integer, Integer>> result = minusTarget2(intArray, 3);
+        if (result == null) {
+            System.out.println("no result found");
+        }
+
+        result.forEach(it -> {
+            System.out.println("" + it.getKey() + it.getValue());
+        });
+    }
+
+    private static List<Pair<Integer, Integer>> minusTarget(int[] arr, int target) {
+        if (arr == null || arr.length == 0) {
+            return null;
+        }
+
+        Arrays.sort(arr);
+        List<Pair<Integer, Integer>> result = new ArrayList<>();
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+            j = Math.max(i + 1, j);
+            while (j < arr.length && arr[j] - arr[i] < target) {
+                j++;
+            }
+
+            if (j == arr.length) {
+                break;
+            }
+
+            if (arr[j] - arr[i] == target) {
+                result.add(new Pair<>(arr[j], arr[i]));
+            }
+        }
+
+        return result;
+    }
+
+    private static List<Pair<Integer, Integer>> minusTarget2(int[] arr, int target) {
+        if (arr == null || arr.length == 0) {
+            return null;
+        }
+
+        List<Pair<Integer, Integer>> result = new ArrayList<>();
+        Arrays.sort(arr);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(target + arr[i])) {
+                result.add(new Pair<>(arr[i], target + arr[i]));
+            }
+
+            if(map.containsKey(arr[i] - target)){
+                result.add(new Pair<>(arr[i], arr[i] - target));
+            }
+            map.put(arr[i], i);
+            map.put(-arr[i], i);
+        }
+
+        return result;
+    }
+}
